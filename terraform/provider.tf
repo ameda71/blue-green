99b8693c5322d106116697a868b6e9d850d5c000
Sukanya992 <sukanya09200206@gmail.com>
2025-04-14 15:47:10 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("account.json") # GCP service account
+  credentials = file("gcp-key") # GCP service account
   project     = var.project_id
   region      = var.region
 }
