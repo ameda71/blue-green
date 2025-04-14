1924986886da2917e236bd2b3880be3a180b835c
Sukanya992 <sukanya09200206@gmail.com>
2025-04-14 15:56:14 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,27 @@
-variable "cluster_name" {
-    default = "cluster-2"
-  
+# Declare Variables
+variable "project_id" {
+  description = "The Google Cloud project ID"
+  type        = string
 }
-variable "region" {
-    default = "us-central1-c"
-  
-}
-variable "node_count" {
-    default = "2"
 
-  
+variable "cluster_name" {
+  description = "The name of the Kubernetes cluster"
+  default     = "cluster-2"
+}
+
+variable "region" {
+  description = "The region for the Kubernetes cluster"
+  default     = "us-central1-c"
+}
+
+variable "node_count" {
+  description = "The number of nodes in the cluster"
+  type        = number  # Change type to number
+  default     = 2
 }
 
 variable "node_machine_type" {
   description = "The type of machine to use for nodes in the Kubernetes cluster"
   type        = string
-  default     = "e2-standard-4"  // Adjust based on the required machine type
+  default     = "e2-standard-4"
 }
