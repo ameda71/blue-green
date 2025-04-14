74128fc23f98879e6f8334511e6b48d538a5ee06
Sukanya992 <sukanya09200206@gmail.com>
2025-04-14 15:50:53 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,19 @@
-variable "project_id" {}
-variable "zone"     {}
 variable "cluster_name" {
-  default = "blue-green-cluster"
+    default = "cluster-2"
+  
+}
+variable "region" {
+    default = "us-central1-c"
+  
+}
+variable "node_count" {
+    default = "2"
+
+  
+}
+
+variable "node_machine_type" {
+  description = "The type of machine to use for nodes in the Kubernetes cluster"
+  type        = string
+  default     = "e2-standard-4"  // Adjust based on the required machine type
 }
