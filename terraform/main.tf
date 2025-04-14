aeeff46b8d38f6cbd6ee8e6316aadb661823107f
Sukanya992 <sukanya09200206@gmail.com>
2025-04-14 16:08:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,11 @@
-# main.tf
 provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
+  zone = var.region
+  project = "plated-epigram-452709-h6"
 }
 
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
-  location = var.zone
+  location = var.region
 
   deletion_protection = true
 
@@ -19,38 +17,4 @@ resource "google_container_cluster" "primary" {
   }
 
   remove_default_node_pool = false
-}
-
-# Variables file
-variable "project_id" {
-  description = "The Google Cloud project ID"
-  type        = string
-}
-
-variable "region" {
-  description = "The region for the Kubernetes cluster"
-  type        = string
-}
-
-variable "zone" {
-  description = "The zone for the Kubernetes cluster"
-  type        = string
-}
-
-variable "cluster_name" {
-  description = "The name of the Kubernetes cluster"
-  type        = string
-  default     = "gke-cluster"
-}
-
-variable "node_count" {
-  description = "The number of nodes in the cluster"
-  type        = number
-  default     = 3
-}
-
-variable "node_machine_type" {
-  description = "The type of machine to use for nodes"
-  type        = string
-  default     = "e2-medium"
 }
